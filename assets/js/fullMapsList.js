f4df968dd178381bbf3988ebb61c85cd3c942cd4
nnngrach <nnngrach@gmail.com>
2020-03-09 02:01:13 +0300

    start experiments with rubricator for maps

---
+++ assets/js/fullMapsList.js
@@ -0,0 +1,10 @@
+var testMapList = {
+  0: {name: "Google Satellite", region: "World", type: "Satellite"},
+  1: {name: "Yandex Satellite", region: "World", type: "Satellite"},
+  2: {name: "Open Street Map", region: "World", type: "City"},
+  3: {name: "2Gis", region: "Russia", type: "City"},
+  4: {name: "Visicom", region: "Ukraine", type: "City"},
+  5: {name: "Open Topo Map", region: "World", type: "Hike"},
+  6: {name: "GGC", region: "Russia", type: "Hike"},
+  7: {name: "Karpaty", region: "Ukraine", type: "Hike"}
+}
---
+++ assets/js/redirecting.js
@@ -0,0 +1,41 @@
+function redirectToIndexPage() {
+    var url = String(location);
+
+    if (url.indexOf("_en") == -1) {
+      url = "/index";
+    } else {
+      url = "/index_en";
+    }
+
+    window.location.replace(url);
+}
+
+
+function redirectToContactsPage() {
+    var url = String(location);
+
+    if (url.indexOf("_en") == -1) {
+      url = "/Web/Html/Contacts_ru";
+    } else {
+      url = "/Web/Html/Contacts_en";
+    }
+
+    window.location.replace(url);
+}
+
+
+function redirectToPageWithAnotherLanguage() {
+    var url = String(location);
+
+    if (url.indexOf("index_en") != -1) {
+      url = url.replace("index_en", "index");
+    } else if (url.indexOf("index") != -1) {
+      url = url.replace("index", "index_en");
+    } else if (url.indexOf("_ru") == -1) {
+      url = url.replace("_en", "_ru");
+    } else {
+      url = url.replace("_ru", "_en");
+    }
+
+    window.location.replace(url);
+}
